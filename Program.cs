43a76b9637f57867aa49c8add73d8b1ef8d82fa8
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-01-10 16:28:38 -0700

    refactoring for web api

--- Program.cs
+++ Program.cs
@@ -10,33 +10,7 @@ namespace ConsoleAdventure
 		public static void Main(string[] args)
 		{
 			Console.Clear();
-			System.Console.WriteLine(@"
-                                                                              
-                                                                              
-MMMMMMMM               MMMMMMMMVVVVVVVV           VVVVVVVVPPPPPPPPPPPPPPPPP   
-M:::::::M             M:::::::MV::::::V           V::::::VP::::::::::::::::P  
-M::::::::M           M::::::::MV::::::V           V::::::VP::::::PPPPPP:::::P 
-M:::::::::M         M:::::::::MV::::::V           V::::::VPP:::::P     P:::::P
-M::::::::::M       M::::::::::M V:::::V           V:::::V   P::::P     P:::::P
-M:::::::::::M     M:::::::::::M  V:::::V         V:::::V    P::::P     P:::::P
-M:::::::M::::M   M::::M:::::::M   V:::::V       V:::::V     P::::PPPPPP:::::P 
-M::::::M M::::M M::::M M::::::M    V:::::V     V:::::V      P:::::::::::::PP  
-M::::::M  M::::M::::M  M::::::M     V:::::V   V:::::V       P::::PPPPPPPPP    
-M::::::M   M:::::::M   M::::::M      V:::::V V:::::V        P::::P            
-M::::::M    M:::::M    M::::::M       V:::::V:::::V         P::::P            
-M::::::M     MMMMM     M::::::M        V:::::::::V          P::::P            
-M::::::M               M::::::M         V:::::::V         PP::::::PP          
-M::::::M               M::::::M          V:::::V          P::::::::P          
-M::::::M               M::::::M           V:::V           P::::::::P          
-MMMMMMMM               MMMMMMMM            VVV            PPPPPPPPPP          
-                                                                              
-                                                                              
-                                                                              
-                                                                              
-                                                                              
-                                                                              
-                                                                              
-			");
+
 			Console.Write("Choose a mode:\n\t- EASY mode\n\t- HARD mode (coming soon)\n> ");
 			string mode = "";
 			while (mode == "")
